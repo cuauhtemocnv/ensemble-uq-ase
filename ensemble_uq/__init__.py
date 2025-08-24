8078ee20297a5b09cb628765c74cd4ac1b84029a
Cuauhtemoc Núñez Valencia <cuau_valencia@hotmail.com>
2025-08-24 14:36:57 -0700

    Create __init__.py

---
+++ ensemble_uq/__init__.py
@@ -0,0 +1,12 @@
+"""
+Ensemble-UQ-ASE
+===============
+
+A small package for ensemble-based uncertainty quantification of interatomic 
+potentials within the ASE (Atomic Simulation Environment) framework.
+"""
+
+from .ensemble_calculator import EnsembleCalculator
+
+__all__ = ["EnsembleCalculator"]
+__version__ = "0.1.0"
