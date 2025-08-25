8fac5448e7e9b05228e55e60cdf575702fa0fb9f
Cuauhtemoc Núñez Valencia <cuau_valencia@hotmail.com>
2025-08-25 11:24:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ensemble-uq-ase",
-    version="0.1.0",
+    version="0.1.1",
     description="Uncertainty quantification with ensembles of interatomic potentials in ASE",
     author="Cuauhtemocnv",
     packages=find_packages(),
